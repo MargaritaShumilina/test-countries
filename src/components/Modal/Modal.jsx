e022ef467fc81253070b33a1584fdbd29e7f2083
Margarita Shumilina <margar.maksim@ya.ru>
2024-07-31 18:03:12 +0300

    ver3

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -17,14 +17,14 @@ function Modal({ countryInfo, onClose }) {
                         <h2>Информация о стране</h2>
                         <div className='modal__info'>
                             <div className='modal__text-info'>
-                                <p><strong>Общее название:</strong> {countryInfo.name.common}</p>
-                                <p><strong>Официальное название:</strong> {countryInfo.name.official}</p>
-                                <p><strong>Столица:</strong> {countryInfo.capital.join(', ')}</p>
-                                <p><strong>Регион:</strong> {countryInfo.region}</p>
-                                <p><strong>Численность населения:</strong> {countryInfo.population}</p>
-                                <p><strong>Языки:</strong> {Object.values(countryInfo.languages).join(', ')}</p>
+                                <p className='modal__text'><strong>Общее название:</strong> {countryInfo.name.common}</p>
+                                <p className='modal__text'><strong>Официальное название:</strong> {countryInfo.name.official}</p>
+                                <p className='modal__text'><strong>Столица:</strong> {countryInfo.capital.join(', ')}</p>
+                                <p className='modal__text'><strong>Регион:</strong> {countryInfo.region}</p>
+                                <p className='modal__text'><strong>Численность населения:</strong> {countryInfo.population}</p>
+                                <p className='modal__text'><strong>Языки:</strong> {Object.values(countryInfo.languages).join(', ')}</p>
                             </div>
-                                <img src={countryInfo.flags.png} alt={`Flag of ${countryInfo.name.common}`} />
+                                <img src={countryInfo.flags.png} alt={`Flag of ${countryInfo.name.common}`} className='modal__img-info'/>
                         </div>
                     </div>
                 </div>
